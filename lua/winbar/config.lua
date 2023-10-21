9a4ad749a4fb0c3f2ea84bbe84f4dceca358185e
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-21 21:02:02 +0300

    feat: format and luarc.json

--- lua/winbar/config.lua
+++ lua/winbar/config.lua
@@ -1,45 +1,45 @@
 local M = {}
 
 M.defaults = {
-    enabled = false,
+	enabled = false,
 
-    show_file_path = true,
-    show_symbols = true,
+	show_file_path = true,
+	show_symbols = true,
 
-    colors = {
-        path = '',
-        file_name = '',
-        symbols = '',
-    },
+	colors = {
+		path = "",
+		file_name = "",
+		symbols = "",
+	},
 
-    icons = {
-        file_icon_default = '',
-        seperator = '>',
-        editor_state = '●',
-        lock_icon = '',
-    },
+	icons = {
+		file_icon_default = "",
+		seperator = ">",
+		editor_state = "●",
+		lock_icon = "",
+	},
 
-    exclude_filetype = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
-        'alpha',
-        'lir',
-        'Outline',
-        'spectre_panel',
-        'toggleterm',
-        'qf',
-    }
+	exclude_filetype = {
+		"help",
+		"startify",
+		"dashboard",
+		"packer",
+		"neogitstatus",
+		"NvimTree",
+		"Trouble",
+		"alpha",
+		"lir",
+		"Outline",
+		"spectre_panel",
+		"toggleterm",
+		"qf",
+	},
 }
 
 M.options = {}
 
 function M.set_options(opts)
-	M.options = vim.tbl_deep_extend('force', {}, M.defaults, opts or {})
+	M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
 end
 
 return M
